5c868e63f2eb438a07994bcb73df0d00db489a9f
Răzvan Trufin <117245410+razvantrufin99x@users.noreply.github.com>
2024-02-16 11:26:13 +0200

    Add project files.

---
+++ descompunere/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace descompunere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 94);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1141, 552);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "finddividingprimes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "findprimes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "prime";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(257, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(775, 9);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(209, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(627, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "dividedbyprimes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(455, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1000, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "descompune";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(627, 38);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(526, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 658);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "primes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ descompunere/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace descompunere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] nrprime = {
+        1,2,3,4,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997
+        };
+        public List<int> fatoridivN = new List<int>();
+
+        public bool ePrim(int x)
+        {
+            bool r = true;
+            for (int i = 2; i < (x / 2); i++)
+            {
+                if (x % i == 0) { r = false;return r; }
+            }
+            return r;
+        }
+        public void findPrimeNumbers(int n, ref TextBox t)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                bool r = ePrim(j);
+                if (r == true) { t.Text += j.ToString() + ","; }
+            }
+        }
+        public int findAllPrimeDividingANumber(int x, ref TextBox t)
+        {
+            fatoridivN = new List<int>();
+            int y = (x / 2);
+            int r = -1;
+            t.Text += x.ToString() + ":";
+            for (int i =  0; i < nrprime.Length; i++)
+            {
+              
+                if (x % nrprime[i] == 0)
+                {
+                    r = nrprime[i]; 
+                    t.Text += nrprime[i].ToString()+",";
+                    fatoridivN.Add(nrprime[i]);
+                }
+            }
+            t.Text += "\r\n";
+            return r;
+        }
+        public void findAllNumbersDivizibleWithAPrime()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                findAllPrimeDividingANumber(i,ref this. textBox1);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            findAllNumbersDivizibleWithAPrime();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            findPrimeNumbers(1000, ref this.textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox4.Text = (ePrim(int.Parse(this.textBox2.Text))).ToString();
+        }
+        public int findBiggesPrimeDivizorOfXInAList(int x, ref List<int> l)
+        { 
+            int r = l.Count-1;
+            for (int i = r; i >= 0; i--)
+            {
+                if (x < r) { return i; }
+            }
+            return r;
+        }
+        public void decompunereNumarInFactoriPrimi()
+        {
+            textBox1.Text = "";
+            int x = int.Parse(this.textBox2.Text);
+            int z = 0;
+            textBox1.Text += x.ToString()+":";
+            while (x > 1)
+            {
+                findAllPrimeDividingANumber(x, ref this.textBox1);
+
+                if (x >= this.fatoridivN[this.fatoridivN.Count - 1])
+                {
+                    z = this.fatoridivN[this.fatoridivN.Count - 1];
+                    textBox5.Text += z.ToString() + ",";
+                }
+                else
+                {
+                    z = this.fatoridivN[findBiggesPrimeDivizorOfXInAList(x, ref this.fatoridivN)];
+                    textBox5.Text += z.ToString() + ",";
+                }
+                
+                
+                x = x / z;
+                textBox1.Text += x.ToString()+",";
+                
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            findAllPrimeDividingANumber(int.Parse(this.textBox2.Text), ref this.textBox3);
+
+            /*
+                574 / 41 =  14
+                14 / 7 = 2
+                2 / 2 = 1
+                1 / 1 = 1
+
+             */
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            decompunereNumarInFactoriPrimi();
+            /*
+                500:100,20,4,1,
+                500:1,2,4,5,
+                500:1,2,4,5,
+                100:1,2,4,5,
+                20:1,2,4,5,
+                4:1,2,4,
+
+             */
+        }
+    }
+}
